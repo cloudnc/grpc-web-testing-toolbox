7d978a713c21c675654b472ab987a48c61e63e82
Maxime Robert <maxime.robert1992@gmail.com>
2022-06-01 10:53:52 +0200

    feat: add implementation for playwright

--- src/playwright/index.ts
+++ src/playwright/index.ts
@@ -1,3 +1,91 @@
-export function add(a: number, b: number) {
-  return a + b;
+import { expect, Page } from '@playwright/test';
+import { grpc } from '@improbable-eng/grpc-web';
+import {
+  GrpcErrorResponse,
+  grpcResponseToBuffer,
+  GrpcSuccessResponse,
+} from '../base';
+import { Request } from 'playwright-core';
+
+export interface UnaryMethodDefinitionish
+  extends grpc.UnaryMethodDefinition<any, any> {
+  requestStream: any;
+  responseStream: any;
+}
+
+export interface MockedGrpcCall {
+  /**
+   * Wait for the mocked request. This is useful if you want to assert on the request body of an RPC call, or if you
+   * need to wait for an endpoint to respond before continuing assertions
+   *
+   * The request message argument to the optional predicate should be used to match the request payload.
+   * Note the requestMessage objects need to be decoded using a protobuf decoder for the specific expected message.
+   */
+  waitForMock(
+    requestPredicate?: (
+      requestMessage: Uint8Array | null,
+      request: Request
+    ) => boolean | Promise<boolean>
+  ): Promise<{ requestMessage: Uint8Array | null }>;
+}
+
+/**
+ * Remove the header information from the request body. This is the reverse of the `frameRequest` function that
+ * gRPC-Web applies to wrap the message body up for transport
+ * @see https://github.com/improbable-eng/grpc-web/blob/53aaf4cdc0fede7103c1b06f0cfc560c003a5c41/client/grpc-web/src/util.ts#L3
+ */
+function unframeRequest(requestBody: Uint8Array): Uint8Array {
+  return new Uint8Array(requestBody).slice(5);
+}
+
+export function mockGrpcUnary(
+  page: Page,
+  rpc: UnaryMethodDefinitionish,
+  response: GrpcSuccessResponse | GrpcErrorResponse
+): MockedGrpcCall {
+  const url = `/${rpc.service.serviceName}/${rpc.methodName}`;
+
+  // note this wildcard route url base is done in order to match both localhost and deployed service usages.
+  page.route('**' + url, (route) => {
+    expect(
+      route.request().method(),
+      'ALL gRPC requests should be a POST request'
+    ).toBe('POST');
+
+    // error messages need to have a zero length message field to be considered valid
+    const grpcResponse = grpcResponseToBuffer(response);
+
+    return route.fulfill({
+      body: grpcResponse,
+      contentType: 'application/grpc-web+proto',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  });
+
+  return {
+    async waitForMock(requestPredicate?) {
+      const request = await page.waitForRequest((req) => {
+        if (!req.url().includes(url)) {
+          return false;
+        }
+
+        if (requestPredicate) {
+          const messageBody = req.postDataBuffer();
+          const unframed = !messageBody ? null : unframeRequest(messageBody);
+          return requestPredicate(unframed, req);
+        }
+
+        return true;
+      });
+
+      await page.waitForResponse((resp) => resp.url().includes(url));
+
+      const messageBody = request.postDataBuffer();
+      const requestMessage = !messageBody ? null : unframeRequest(messageBody);
+
+      return { requestMessage };
+    },
+  };
 }
